05c591dff89b3228911af69f7b9f43e512846af6
Siddique-Ahmed <itssiddique786@gmail.com>
2024-10-30 02:44:35 +0500

    modal and drawer completed

--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -1,7 +1,14 @@
+import { CourseTable } from "@/components/dataTables/CourseTable";
+import { CourseDialog } from "@/components/modals/CourseModal";
+
 export default function Courses(){
   return(
     <div className="min-h-screen p-10">
+      <div className="w-full flex justify-between">
       <h1 className="text-center font-bold text-4xl">Courses</h1>
+      <CourseDialog/>
+      </div>
+      <CourseTable/>
     </div>
   )
 }
---
+++ src/components/modals/CourseModal.js
@@ -0,0 +1,105 @@
+"use client";
+import * as React from "react";
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+// import { useMediaQuery } from "@/hooks/use-media-query";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function CourseDialog() {
+  const [open, setOpen] = useState(false);
+  const isDesktop = true;
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Add Courses</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Courses</DialogTitle>
+          </DialogHeader>
+          <ProfileForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Add Courses</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Add Courses</DrawerTitle>
+        </DrawerHeader>
+        <ProfileForm className="px-4" />
+        <DrawerFooter className="pt-2">
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+function ProfileForm({ className }) {
+  return (
+    <form className={cn("grid items-start gap-4", className)}>
+      <div className="grid gap-2">
+        <Label htmlFor="course_name">Course Name</Label>
+        <Input required type="text" id="course_name" />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="duration">Duration</Label>
+        <Input required id="duration" />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="description">Description</Label>
+        <Input required id="description" />
+      </div>
+      <div className="grid gap-2">
+        <Select required>
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="active" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="active">Active</SelectItem>
+            <SelectItem value="not-active">Not Active</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <Button type="submit">Add Course</Button>
+    </form>
+  );
+}
