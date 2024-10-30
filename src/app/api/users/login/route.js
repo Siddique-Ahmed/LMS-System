1a6fd5acdedf417bd53b45294f568093bbbaeda0
Siddique-Ahmed <itssiddique786@gmail.com>
2024-10-30 19:55:44 +0500

    Last thing is diploy env file

---
+++ src/app/api/users/login/route.js
@@ -0,0 +1,41 @@
+import { UserModel } from "@/lib/Collections/UserModal";
+import { connectDB } from "@/lib/dbConnect";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+export async function POST(request) {
+  await connectDB();
+  const obj = await request.json();
+  const user = await UserModel.findOne({ email: obj.email });
+  if (!user)
+    return Response.json(
+      { error: true, msg: "User Not Found" },
+      { status: 404 }
+    );
+  const isPasswordMatch = await bcrypt.compare(obj.password, user.password);
+  if (!isPasswordMatch)
+    return Response.json(
+      {
+        error: true,
+        msg: "Password is not valid",
+      },
+      { status: 404 }
+    );
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_KEY
+  );
+
+  return Response.json(
+    {
+      error: false,
+      msg: "User Login Successfully",
+      user,
+      token,
+    },
+    { status: 200 }
+  );
+}
